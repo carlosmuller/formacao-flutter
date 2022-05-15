e400d4e63d29dcd2340fac3ccea87caeefa3ccae
Carlos Müller <kdu428@gmail.com>
2022-05-15 08:24:13 -0300

    chore: adiociona timeout no cliente http

--- lib/http/cliente_web.dart
+++ lib/http/cliente_web.dart
@@ -6,9 +6,13 @@ import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 class Cliente {
+  final String _host = 'http://192.168.15.13:8080';
+  final Duration? timeout;
+
+  Cliente({this.timeout});
   Future<Response> get(String uri) async{
     Client client = getHttpClient();
-    return client.get(Uri.tryParse(uri)!);
+    return client.get(Uri.tryParse('$_host$uri')!).timeout(timeout?? const Duration(seconds: 5));
   }
 
   Client getHttpClient() {
