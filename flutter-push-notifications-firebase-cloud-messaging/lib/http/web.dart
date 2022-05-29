f175e7674bb0264f8adac749084b2354c38d9935
Carlos Müller <kdu428@gmail.com>
2022-05-29 15:22:10 -0300

    chore: configurando para cadastrar dispositivo

--- flutter-push-notifications-firebase-cloud-messaging/lib/http/web.dart
+++ flutter-push-notifications-firebase-cloud-messaging/lib/http/web.dart
@@ -1,20 +1,34 @@
 import 'dart:convert';
-import 'package:meetups/models/event.dart';
+
 import 'package:http/http.dart' as http;
+import 'package:meetups/models/device.dart';
+import 'package:meetups/models/event.dart';
 
 const String baseUrl = 'http://192.168.15.13:8080/api';
 
 Future<List<Event>> getAllEvents() async {
-  final response = await http
-      .get(Uri.parse('$baseUrl/events'));
+  final response = await http.get(Uri.parse('$baseUrl/events'));
 
   if (response.statusCode == 200) {
     final List<dynamic> decodedJson = jsonDecode(response.body);
     return decodedJson.map((dynamic json) => Event.fromJson(json)).toList();
-  }
-  else {
+  } else {
     throw Exception('Falha ao carregar os eventos');
   }
+}
 
+Future<http.Response> sendDevice(Device device) async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/devices'),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
 
+    },
+    body:  jsonEncode({
+      'token': device.token??'',
+      'modelo': device.model??'',
+      'marca': device.brand??''
+    })
+  );
+  return response;
 }
--- flutter-push-notifications-firebase-cloud-messaging/lib/main.dart
+++ flutter-push-notifications-firebase-cloud-messaging/lib/main.dart
@@ -1,21 +1,47 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:meetups/screens/events_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:meetups/models/device.dart';
+import 'package:meetups/screens/events_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
+import 'http/web.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  String? token = await messaging.getToken();
+  setPushToken(token);
   runApp(App());
+}
+
+void setPushToken(String? token) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? prefsToken = prefs.getString('push-token');
+  if (token != prefsToken && token != null) {
+    await prefs.setString('push-token', token);
+    if(!(prefs.getBool('token-sent')?? false)){
+      final deviceInfoPlugin = DeviceInfoPlugin();
+      final deviceInfo = await deviceInfoPlugin.deviceInfo;
+      final map = deviceInfo.toMap();
+      final device =
+      Device(brand: map['brand'], model: map['model'], token: token);
+      sendDevice(device)
+          .then((value) async => await prefs.setBool('token-sent', true))
+          .catchError((error) async => await prefs.setBool('token-sent', false));
+      print("salvei novamente");
+    }
+  }
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dev meetups',
