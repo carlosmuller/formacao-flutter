04d295b8e37fe262f3ddfa12ee944cffa07cb192
Carlos Müller <kdu428@gmail.com>
2022-05-29 08:24:25 -0300

    chore: tipando os parametros das rotas

--- nuvigator/lib/routes/package_details_route.dart
+++ nuvigator/lib/routes/package_details_route.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:nuvigator/next.dart';
+import 'package:proj/models/package_model.dart';
+import 'package:proj/models/producer_model.dart';
 import 'package:proj/screens/package_details_screen.dart';
 
-class PackageDetailsRoute extends NuRoute {
-  @override
-  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
-    return PackageDetailsScreen(
-      producer: settings.rawParameters['producer'],
-      package: settings.rawParameters['package'],
+class PackageDetailsArgs {
+  final Package package;
+  final Producer producer;
+
+  PackageDetailsArgs({
+    @required this.package,
+    @required this.producer,
+  });
+
+  static PackageDetailsArgs fromJson(Map<String, dynamic> json) {
+    return PackageDetailsArgs(
+      package: json['package'],
+      producer: json['producer'],
     );
   }
+}
+
+class PackageDetailsRoute
+    extends NuRoute<NuRouter, PackageDetailsArgs, String> {
+  @override
+  Widget build(
+      BuildContext context, NuRouteSettings<PackageDetailsArgs> settings) {
+    return PackageDetailsScreen(
+      producer: settings.args.producer,
+      package: settings.args.package,
+    );
+  }
+
+  @override
+  ParamsParser<PackageDetailsArgs> get paramsParser =>
+      PackageDetailsArgs.fromJson;
 
   @override
   String get path => 'package-details';
--- nuvigator/lib/routes/producer_details_route.dart
+++ nuvigator/lib/routes/producer_details_route.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:nuvigator/next.dart';
+import 'package:proj/models/producer_model.dart';
 import 'package:proj/routes/package_details_route.dart';
 import 'package:proj/screens/producer_details_screen.dart';
 
-class ProducerDetailsRoute extends NuRoute {
+class ProducerDetailsRoute extends NuRoute<NuRouter, ProducerDetailsArgs, String> {
   @override
-  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
+  Widget build(BuildContext context, NuRouteSettings<ProducerDetailsArgs> settings) {
     return ProducerDetailsScreen(
-      producer: settings.rawParameters['producer'],
+      producer: settings.args.producer,
       onPackageClick: (parameters) =>
           nuvigator.open(PackageDetailsRoute().path, parameters: parameters),
     );
@@ -18,4 +19,18 @@ class ProducerDetailsRoute extends NuRoute {
 
   @override
   ScreenType get screenType => materialScreenType;
+
+  @override
+  ParamsParser<ProducerDetailsArgs> get paramsParser => ProducerDetailsArgs.fromJson;
+
+}
+
+class ProducerDetailsArgs {
+  final Producer producer;
+
+  ProducerDetailsArgs({@required this.producer});
+
+  static ProducerDetailsArgs fromJson(Map<String, dynamic> json){
+    return ProducerDetailsArgs(producer: json['producer']);
+  }
 }
